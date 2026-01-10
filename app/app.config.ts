6c6f4a06c8728f7cf6602eaa2ce1df739ebbe5f7
卢东东 <xjoylu@gmail.com>
2026-01-10 17:17:41 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   title: 'AAG',
   email: 'xjoylu@gmail.com',
   github: 'https://github.com/xjoylu',
-  twitter: 'https://x/xjoylu',
+  twitter: 'https://x.com/xjoylu',
   telegram: 'https://ygo.cc',
   blog: 'https://lcc.cc',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
